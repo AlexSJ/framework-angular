a39110aacb07f2849f5e8b3f30ae3212f8d18128
Alex S. Juchem <alex.juchem@solucoesglobais.com.br>
2014-12-22 17:32:15 -0200

    Criando controllers e routers

---
+++ framework/js/app.js
@@ -0,0 +1,17 @@
+var frameworkApp = angular.module('frameworkApp', []);
+
+frameworkApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+		when('/phones', {
+			templateUrl: 'partials/phone-list.html',
+			controller: 'PhoneListCtrl'
+		}).
+		when('/phones/:phoneId', {
+			templateUrl: 'partials/phone-detail.html',
+			controller: 'PhoneDetailCtrl'
+		}).
+		otherwise({
+			redirectTo: '/phones'
+	});
+}]);
--- framework/view/layout.php
+++ framework/view/layout.php
@@ -1,15 +1,24 @@
 <!doctype html>
-<html lang="en" ng-app>
+<html lang="en" ng-app="frameworkApp">
 	<head>
 		<meta charset="utf-8">
 		<title>My HTML File</title>
 		<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
 		<link rel="stylesheet" href="css/app.css">
-		<script src="assets/js/angular.min.js"></script>
 	</head>
-	<body>
+	<body ng-controller="homeCtrl">
 
-	  <p>Nothing here {{2 + 3}}</p>
+		<ul>
+			<li ng-repeat="phone in phones">
+				<span>{{phone.name}}</span>
+				<p>{{phone.snippet}}</p>
+			</li>
+		</ul>
 
 	</body>
+
+	<script src="assets/js/angular.min.js"></script>
+	<script src="framework/js/app.js"></script>
+	<script src="framework/js/controllers/homeCtrl.js"></script>
+
 </html>
